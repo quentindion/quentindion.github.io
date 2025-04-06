9d589cf0ff9ac1b2cc95e0ae7199c7928e0997c7
Quentin Dion <19429140+quentindion@users.noreply.github.com>
2025-04-06 10:23:29 +0000

    Fix deps

--- src/useTheme.ts
+++ src/useTheme.ts
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react";
-import { useLocalStorage } from "usehooks-ts";
+import { useState } from "react";
 
 export type Theme = undefined | "light" | "dark";
 
 export default function useTheme() {
-    
-    const [themeState, setThemeState] = useState<Theme>();
-    const [storedTheme, setStoredTheme, removeStoredTheme] = useLocalStorage<Theme>("theme", undefined, {
-        serializer: value => `${value}`,
-        deserializer: value => value as Theme,
-    });
 
-    const detectTheme = (): Theme => (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) ? "dark" : "light";
+    const [theme, setTheme] = useState<Theme>(window.isDarkMode() ? "dark" : "light");
 
     const toggleTheme = () => {
-        if(storedTheme === undefined && window.matchMedia("screen and (prefers-color-scheme: dark)").matches)
-            setStoredTheme("light");
-        else if(storedTheme === undefined && !window.matchMedia("screen and (prefers-color-scheme: dark)").matches)
-            setStoredTheme("dark");
-        else
-            removeStoredTheme();
+
+        const storedTheme = (localStorage.getItem("theme") ?? undefined) as Theme;
+
+        if(storedTheme === undefined && window.matchMedia("screen and (prefers-color-scheme: dark)").matches) {
+            localStorage.setItem("theme", "light");
+            setTheme("light");
+        } else if(storedTheme === undefined && !window.matchMedia("screen and (prefers-color-scheme: dark)").matches) {
+            localStorage.setItem("theme", "dark");
+            setTheme("dark");
+        } else {
+            localStorage.removeItem("theme");
+            setTheme(undefined);
+        }
+
+        console.log(
+            localStorage.getItem("theme")
+        )
 
         window.applyTheme();
     }
 
-    useEffect(() => { setThemeState(storedTheme); }, [storedTheme]);
-    useEffect(() => { setThemeState(detectTheme()); }, []);
-
-    return [themeState, toggleTheme] as [typeof themeState, typeof toggleTheme];
+    return [theme, toggleTheme] as [typeof theme, typeof toggleTheme];
 }
